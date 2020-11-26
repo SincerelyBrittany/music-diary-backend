a521f748612523b36e04c912594774a87669c156
SincerelyBrittany <sincerelybr@gmail.com>
2020-11-25 20:36:21 -0500

    adds routes to songs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
-Rails.application.routes.draw do
+# Rails.application.routes.draw do
+  # resources :entries
+  # resources :songs
+  # resources :users
+
+  Rails.application.routes.draw do
+    namespace :api do
+      namespace :v1 do
+        resources :songs do
+          collection do
+            get :top_100
+            get :random
+            get :search
+          end
+        end
+      end
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+# end
