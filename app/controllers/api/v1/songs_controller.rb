86ba366e1117a3dd427773e3fe50fb867333e505
SincerelyBrittany <sincerelybr@gmail.com>
2020-11-29 09:34:39 -0500

    adds the songs controller

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -7,9 +7,9 @@ class Api::V1::SongsController < ApplicationController
     def search
         s_songs = RSpotify::Track.search(params[:q])
         @songs = s_songs.map do |s_song|
-          Song.new_from_spotify_song(s_song)
+          Song.new_from_spotify_song(s_song) 
         end
-    render json: @songs
+      render json: @songs
     end
 
 
