dab5426d74795da8288fd72acb7220caf3268fe0
SincerelyBrittany <sincerelybr@gmail.com>
2020-11-30 19:38:48 -0500

    adds user index, create for users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: users.to_json
+    end 
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            token = encode_token(user.id)
+            render json: { user: UserSerializer.new(user), token: token }
+        else
+            render json: { errors: user.errors.full_messages.to_sentence} 
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:email, :username, :password)
+    end
 end
