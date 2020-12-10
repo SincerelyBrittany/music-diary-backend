076749e5690cf53da9f8e919baa7e5eb09c5fba3
SincerelyBrittany <sincerelybr@gmail.com>
2020-12-09 22:31:42 -0500

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,19 @@
   # resources :entries
   # resources :songs
   # resources :users
-
   Rails.application.routes.draw do
+
+
+  post "/api/v1/login", to: "api/v1/sessions#create"
+  post "/api/v1/signup", to: "api/v1/users#create"
+  delete "/api/v1/logout", to: "api/v1/sessions#destroy"
+  post "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
     namespace :api do
       namespace :v1 do
+        resources :users
         resources :songs do
           collection do
-            get :top_100
-            get :random
-            get :search
+            post :search
           end
         end
       end
