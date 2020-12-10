714c14e3fa387998323521456fa3ac958ce450c1
SincerelyBrittany <sincerelybr@gmail.com>
2020-12-09 22:26:56 -0500

    adds search and renders songs

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -5,11 +5,12 @@ class Api::V1::SongsController < ApplicationController
     end
 
     def search
-        s_songs = RSpotify::Track.search(params[:q])
+        s_songs = RSpotify::Track.search(params[:search])
         @songs = s_songs.map do |s_song|
           Song.new_from_spotify_song(s_song) 
         end
       render json: @songs
+      # render json: { songs: SongSerializer.new(@songs) }
     end
 
 
