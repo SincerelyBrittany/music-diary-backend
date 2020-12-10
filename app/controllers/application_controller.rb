8ef350faaa7b84ca05323dd0c51ce5ca454af1d9
SincerelyBrittany <sincerelybr@gmail.com>
2020-12-09 22:31:30 -0500

    creates tokens and sets tokens for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::API
+    def get_auth_token
+        request.headers["Authorization"]
+    end
+
+    def decode_token
+        begin
+            JWT.decode(get_auth_token, ENV["JWT_KEY"])[0]["user_id"]
+        rescue
+            nil
+        end
+    end
+
+    def session_user
+        User.find_by_id(decode_token)
+    end
+
+    def logged_in_user
+        User.find_by_id(decode_token)
+      end
+
+    def encode_token(id)
+        JWT.encode({user_id: id}, ENV["JWT_KEY"])
+    end
 end
