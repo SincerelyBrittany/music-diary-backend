7e458f1485c8e5e3778e5d39d547379a4550a7b4
SincerelyBrittany <sincerelybr@gmail.com>
2020-12-12 16:42:10 -0500

    updates entry serializer to show user and song

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::API
 
     def logged_in_user
         User.find_by_id(decode_token)
-      end
+    end
 
     def encode_token(id)
         JWT.encode({user_id: id}, ENV["JWT_KEY"])
--- app/serializers/entry_serializer.rb
+++ app/serializers/entry_serializer.rb
@@ -1,3 +1,20 @@
-class EntrySerializer < ActiveModel::Serializer
-  attributes :id
-end
+# class EntrySerializer < ActiveModel::Serializer
+#   attributes :id
+#   belongs_to :user, serializer: UserSerializer
+#   belongs_to :song
+# end
+
+class EntrySerializer
+  def initialize(entry_obj)
+    @entry_obj = entry_obj
+  end 
+
+  def to_serialized_json
+    @entry_obj.to_json(:only => [:id, :description, :user_id, :song_id], methods: :update_date, 
+    :include => {
+        # :entry => {:only => [:name, :description], methods: :time_updated},
+        :song => {:only => [:name, :id, :artist, :image, :preview, :spotify_id, :lyrics]},
+        :user => {:only => [:username]}
+    })
+  end 
+end 
